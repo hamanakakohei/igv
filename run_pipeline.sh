33eb6319de6476c0d70ad85ec0635ed1608d28ac
hamanakakohei <46100957+hamanakakohei@users.noreply.github.com>
2025-08-19 11:34:36 +0900

    Update run_pipeline.sh

--- run_pipeline.sh
+++ run_pipeline.sh
@@ -6,10 +6,6 @@ conda activate misc
 
 # 0. 撮影時に移すファイルなどを指定する
 BED_SNAPSHOT=data/region_of_interest.bed
-#BED_SNAPSHOT=data/tx_genic_mono-exon_918.bed
-#BED_SNAPSHOT=data/tx_genic_multi-exon_237.bed
-#BED_SNAPSHOT=data/tx_nnc_ir_2764.bed
-#BED_SNAPSHOT=data/tx_nnc_new_ss_14197.bed
 ANALYSIS_NAME=aaa
 REF=data/tmp.fa # 名前をこのようにすればRefseq Selectを勝手に読み込まれなくて済む
 MARGIN=5000
@@ -17,13 +13,6 @@ MARGIN=5000
 BASE_FILES=(
   data/aaa.gtf
   data/gencode.v47.primary_assembly.annotation.sorted.gtf
-  data/gm24_gencodev47.chr.scaffold.444141isoforms.geneid_corrected.novel_55580.sorted.gtf
-  data/gm24_gencodev47.chr.scaffold.444141isoforms.geneid_corrected.nnc_ir_2764.sorted.gtf
-  data/gm24_gencodev47.chr.scaffold.444141isoforms.geneid_corrected.nnc_new_ss_14197.sorted.gtf
-  data/gm24_gencodev47.chr.scaffold.444141isoforms.geneid_corrected.nnc_new_ss_14197.mod.sorted.gtf
-  data/gm24_gencodev47.chr.scaffold.444141isoforms.geneid_corrected.novel_loci_2202.gsorted.tf
-  data/gm24_gencodev47.chr.scaffold.444141isoforms.geneid_corrected.genic_mono-exon_918.sorted.gtf
-  data/gm24_gencodev47.chr.scaffold.444141isoforms.geneid_corrected.genic_multi-exon_237.sorted.gtf
 )
 
 IGV_JIKKOU_SCRIPT=~/IGV_Linux_2.19.5/igv.sh
